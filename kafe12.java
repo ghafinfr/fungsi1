311af7fc3d0417e6a8a080321c4b6e10f36c4cb4
“ghafinfr” <ghafinfr@gmail.com>
2025-11-27 08:05:02 +0700

    2.1kafe

---
+++ kafe12.java
@@ -0,0 +1,18 @@
+
+public class kafe12 {
+    public static void main(String[] args) {
+        Menu();
+    }
+
+    public static void Menu() {
+        System.out.println("====MENU RESTO KAFE===");
+        System.out.println("1. KOPI HITAM - RP 15,000");
+        System.out.println("2. CAPPUCINO - RP 20,000");
+        System.out.println("3. LATTE - RP 22,000");
+        System.out.println("4.  TEH TARIK - RP 12,000");
+        System.out.println("5. ROTI GORENG - RP 10,000");
+        System.out.println("6. MIE GORENG - RP 18,000");
+        System.out.println("===========================");
+        System.out.println("SILAHKAN PILIH MENU ANDA INGINKAN. ");
+    }
+}
